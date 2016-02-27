ff582126748c2506b971741b8868c3b03b14a0ef
Michel Jouvin <jouvin@lal.in2p3.fr>
2016-02-27 12:32:20 +0100

    Sphinx config: Exclude twiki_pages/migrated when building

--- source/conf.py
+++ source/conf.py
@@ -72,7 +72,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['twiki_pages/outdated']
+exclude_patterns = ['twiki_pages/outdated', 'twiki_pages/migrated']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
